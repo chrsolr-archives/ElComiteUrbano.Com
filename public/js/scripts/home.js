161b28aff13fb94998da0f35a83bc38b2b09cb98
Christian Soler <christiansoler@Christians-MacBook-Pro.local>
2017-02-17 23:02:48 -0600

    ...

--- public/js/scripts/home.js
+++ public/js/scripts/home.js
@@ -6,30 +6,8 @@ define(["require", "exports", "jquery", "bootstrap"], function (require, exports
             $(document).ready(function () {
                 var SLIDE_SHOW_TIME = 5000;
                 $('.carousel').carousel({ interval: SLIDE_SHOW_TIME, pause: null });
-                $('.loading-bar').each(function (i, element) {
-                    _this.startProgressBar($(element), SLIDE_SHOW_TIME, 10);
-                    $('.carousel').on('slide.bs.carousel', function () {
-                        _this.startProgressBar($(element), SLIDE_SHOW_TIME, 10);
-                    });
-                });
             });
         }
-        HomeScript.prototype.startProgressBar = function ($element, time, delta) {
-            var counter = 0;
-            var width = 0;
-            var incrementWidth = setInterval(progress, delta);
-            function progress() {
-                counter += delta;
-                width = width + 100 / (time / delta);
-                if (counter >= time) {
-                    counter = 0;
-                    width = 0;
-                    clearInterval(incrementWidth);
-                }
-                $element.css({ width: width + "%" });
-            }
-            ;
-        };
         return HomeScript;
     }());
     return HomeScript;
--- public/typescript/scripts/home.ts
+++ public/typescript/scripts/home.ts
@@ -14,35 +14,35 @@ class HomeScript {
 
             $('.carousel').carousel({ interval: SLIDE_SHOW_TIME, pause: null });
 
-            $('.loading-bar').each(function (i, element) {
-                _this.startProgressBar($(element), SLIDE_SHOW_TIME, 10);
-                
-                $('.carousel').on('slide.bs.carousel', function () {
-                    _this.startProgressBar($(element), SLIDE_SHOW_TIME, 10);
-                });
-            });
+            // $('.loading-bar').each(function (i, element) {
+            //     _this.startProgressBar($(element), SLIDE_SHOW_TIME, 10);
+
+            //     $('.carousel').on('slide.bs.carousel', function () {
+            //         _this.startProgressBar($(element), SLIDE_SHOW_TIME, 10);
+            //     });
+            // });
         });
     }
 
-    startProgressBar($element: any, time: int, delta: int): Function {
-        let counter = 0;
-        let width = 0;
+    // startProgressBar($element: any, time: int, delta: int): Function {
+    //     let counter = 0;
+    //     let width = 0;
 
-        const incrementWidth = setInterval(progress, delta);
+    //     const incrementWidth = setInterval(progress, delta);
 
-        function progress() {
-            counter += delta;
-            width = width + 100 / (time / delta);
+    //     function progress() {
+    //         counter += delta;
+    //         width = width + 100 / (time / delta);
             
-            if (counter >= time) {
-                counter = 0;
-                width = 0;
-                clearInterval(incrementWidth);
-            }
+    //         if (counter >= time) {
+    //             counter = 0;
+    //             width = 0;
+    //             clearInterval(incrementWidth);
+    //         }
 
-            $element.css({ width: `${width}%`);
-        };
-    }
+    //         $element.css({ width: `${width}%`);
+    //     };
+    // }
 }
 
 export = HomeScript;
