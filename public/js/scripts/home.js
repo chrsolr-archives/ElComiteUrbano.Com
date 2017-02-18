ed3d9b3c4eb2a494e543a767ab8311af2ee86cdd
Christian Soler <christiansoler@Christians-MacBook-Pro.local>
2017-02-17 22:58:02 -0600

    ...

--- public/js/scripts/home.js
+++ public/js/scripts/home.js
@@ -4,11 +4,30 @@ define(["require", "exports", "jquery", "bootstrap"], function (require, exports
         function HomeScript() {
             var _this = this;
             $(function () {
-                _this.setSlideShow();
+                var SLIDE_SHOW_TIME = 5000;
+                $('.carousel').carousel({ interval: SLIDE_SHOW_TIME, pause: null });
+                $('.loading-bar').each(function (i, element) {
+                    $('.carousel').on('slide.bs.carousel', function () {
+                        _this.startProgressBar($(element), SLIDE_SHOW_TIME, 10);
+                    });
+                });
             });
         }
-        HomeScript.prototype.setSlideShow = function () {
-            $('.carousel').carousel({ interval: 3000, pause: null });
+        HomeScript.prototype.startProgressBar = function ($element, time, delta) {
+            var counter = 0;
+            var width = 0;
+            var incrementWidth = setInterval(progress, delta);
+            function progress() {
+                counter += delta;
+                width = width + 100 / (time / delta);
+                if (counter >= time) {
+                    counter = 0;
+                    width = 0;
+                    clearInterval(incrementWidth);
+                }
+                $element.css({ width: width + "%" });
+            }
+            ;
         };
         return HomeScript;
     }());
--- public/typescript/scripts/home.ts
+++ public/typescript/scripts/home.ts
@@ -4,14 +4,42 @@
 import * as $ from 'jquery';
 
 class HomeScript {
+
+
     constructor() {
+        const _this = this;
+
         $(() => {
-            this.setSlideShow();
+            const SLIDE_SHOW_TIME: int = 5000;
+
+            $('.carousel').carousel({ interval: SLIDE_SHOW_TIME, pause: null });
+
+            $('.loading-bar').each(function (i, element) {
+                $('.carousel').on('slide.bs.carousel', function () {
+                    _this.startProgressBar($(element), SLIDE_SHOW_TIME, 10);
+                });
+            });
         });
     }
 
-    setSlideShow(): void {
-        $('.carousel').carousel({ interval: 3000, pause: null });
+    startProgressBar($element: any, time: int, delta: int): Function {
+        let counter = 0;
+        let width = 0;
+
+        const incrementWidth = setInterval(progress, delta);
+
+        function progress() {
+            counter += delta;
+            width = width + 100 / (time / delta);
+            
+            if (counter >= time) {
+                counter = 0;
+                width = 0;
+                clearInterval(incrementWidth);
+            }
+
+            $element.css({ width: `${width}%`);
+        };
     }
 }
 
