c1fdc795995534a200367bdeeb0eccb230356189
Christian Soler <christiansoler@Christians-MacBook-Pro.local>
2017-02-19 10:15:40 -0600

    updated

--- public/js/modules/navbar.js
+++ public/js/modules/navbar.js
@@ -5,12 +5,14 @@ define(["require", "exports", "jquery"], function (require, exports, $) {
             var _this = this;
             var $toggle_button = $('.navbar-menu-icon');
             _this.$sidebar = $('.navbar-sidebar-wrapper');
+            _this.$body = $('body');
             $toggle_button.on('click', function (e) {
                 _this.toggle();
             });
         }
         NavBar.prototype.toggle = function () {
             this.$sidebar.toggleClass('navbar-sidebar-toggle');
+            this.$body.toggleClass('no-scroll');
         };
         return NavBar;
     }());
--- public/typescript/modules/navbar.ts
+++ public/typescript/modules/navbar.ts
@@ -4,12 +4,14 @@ import * as $ from 'jquery';
 
 class NavBar {
     $sidebar: any;
+    $body: any;
 
     constructor() {
         const _this = this;
         const $toggle_button = $('.navbar-menu-icon');
 
         _this.$sidebar = $('.navbar-sidebar-wrapper');
+        _this.$body = $('body');
 
         $toggle_button.on('click', (e) => {
             _this.toggle();
@@ -18,6 +20,7 @@ class NavBar {
 
     toggle(): void {
         this.$sidebar.toggleClass('navbar-sidebar-toggle');
+        this.$body.toggleClass('no-scroll');
     }
 }
 
